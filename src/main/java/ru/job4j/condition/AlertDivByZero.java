34d6ae8a37e6c1e903b4c2f488a5b77bb4b25d11
Vua3XD <gadekoner@gmail.com>
2023-10-05 20:58:20 +0300

    4.1. Операторы ветвлений [#3611]

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -11,7 +11,7 @@ public class AlertDivByZero {
     }
 
     public static void main(String[] args) {
-        AlertDivByZero.possibleDiv(4);
+        AlertDivByZero.possibleDiv(0);
         AlertDivByZero.possibleDiv(-3);
     }
 }
