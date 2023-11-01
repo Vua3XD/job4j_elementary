5d9327a81373effcd862f3496989f0a3935d3616
Vua3XD <gadekoner@gmail.com>
2023-11-01 09:49:14 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,11 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Rulon Oboev";
+        names[1] = "Sergey Vyazovik";
+        names[2] = "Ivanov Ivan";
+        names[3] = "Petrov Petr";
         System.out.println("Размер массива равен:" + ages.length);
         System.out.println("Размер массива равен:" + surnames.length);
         System.out.println("Размер массива равен:" + prices.length);
